7d5dde76825eb1058902db970d8abbdf65034860
Tima5028 <116207769+Tima5028@users.noreply.github.com>
2022-11-04 19:46:45 +0300

    Add files via upload

---
+++ HW6.py
@@ -0,0 +1,41 @@
+# 3450
+# a=input("Введите число: ")
+# print(int(a*50)**2)
+
+
+# 3458
+# a=int(input("Введите количесвто яблок: "))
+# b=int(input("Введите количесвто школьников: "))
+# print(f"Каждый школьник получит {a//b} яблока")
+
+
+# 3507
+# a=int(input("Введите 1 число: "))
+# b=int(input("Введите 2 число: "))
+# c=int(input("Введите 3 число: "))
+# if a==b and a==c and b==c:
+#     print("3")
+# elif a==b or a==c or b==c:
+#     print("2")
+# elif a!=b and a!=c and b!=c:
+#     print("0")
+
+# 3508
+
+# 3739
+# s = input("Введите строку: ")
+# a = s.find('f')
+# b = s.rfind('f')
+# if a == b:
+#     print(a)
+# else:
+#     print(a, b)
+
+# 3741
+# s = input("Введите строку: ")
+# a = s.find('h')
+# b = s.rfind('h')
+# s=list(s)
+# del s[a:b]
+# s="".join(s)
+# print(s)
